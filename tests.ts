9aff156a1cc4eb9ba3363b8a030dc2949a260063
SignpostMarv <me@signpostmarv.name>
2024-06-30 08:15:13 +0000

    initial commit
    avoid having to set up the same files for new projects

---
+++ tests.ts
@@ -0,0 +1,29 @@
+import {
+	tap,
+} from 'node:test/reporters';
+import {
+	run,
+} from 'node:test';
+
+const __dirname = import.meta.dirname;
+
+const ac = new AbortController();
+
+let already_stopped = false;
+
+run({
+	files: [
+	],
+	concurrency: true,
+	signal: ac.signal,
+})
+	.on('test:fail', (e) => {
+		ac.abort();
+		if (!already_stopped) {
+			console.error(e);
+		}
+		already_stopped = true;
+		process.exitCode = 1;
+	})
+	.compose(tap)
+	.pipe(process.stdout);
